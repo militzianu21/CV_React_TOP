d1b897bc98c58fb83b65aaa0524a5d8640e87f2f
militzianu21 <89782953+militzianu21@users.noreply.github.com>
2024-10-16 20:07:53 +0300

    Update WorkSectionPlain.jsx

--- src/components/WorkSectionPlain.jsx
+++ src/components/WorkSectionPlain.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "./Button";
-import PanelPlain from "./panelPlain";
+import PanelPlain from "./PanelPlain";
 const names = {
   institution: "companyName",
   title: "title",
